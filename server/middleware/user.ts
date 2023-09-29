916ce735faaf07320d5e4b621b514a204c206ffe
drigoalexander <drigosihombinga@gmail.com>
2023-09-30 03:50:58 +0700

    try fixing prod error

--- server/middleware/user.ts
+++
@@ -1,6 +0,0 @@
-// import { serverSupabaseUser } from "#supabase/server";
-
-// export default defineEventHandler((event) => {
-//   const user = serverSupabaseUser(event);
-//   event.context.user = user;
-// });
